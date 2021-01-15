81074eb07adb63ac271b104e5f0bdc08f7bc5cdf
Nami Falhan <namfaldev@gmail.com>
2021-01-15 16:58:44 +0100

    set up vue router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <div id="app">
-    <Home></Home>
+    <router-view />
   </div>
 </template>
 
 <script>
-import Home from './views/Home.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Home
-  }
+  name: 'App'
 }
 </script>
 
--- src/router.js
+++ src/router.js
@@ -1,1 +1,19 @@
-import {createWebHistory} from 'vue-router'
+import VueRouter from 'vue-router'
+import Home from './views/Home'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home
+    }
+]
+
+const router = new VueRouter({ mode: 'history', routes })
+
+export default router
