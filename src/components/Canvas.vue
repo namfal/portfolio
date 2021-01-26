e52fe4256bf0e821adb4684a853214eb1e6b2d7c
Nami Falhan <namfaldev@gmail.com>
2021-01-26 18:09:55 +0100

    added a subtle animation to the canvas svgs

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -28,6 +28,26 @@
                 let max = Math.floor(upperLimit)
                 return Math.floor(Math.random() * (max - min + 1) + min)
             },
+            pickBetweenTwo (num1, num2) {
+                return (this.getRandomNumber(0, 2) % 2) === 0 ? num1 : num2
+            },
+            getTranslationCoordinates (x, y) {
+              let heightOrWidth = this.pickBetweenTwo(this.width, this.height)
+              let num1, num2
+
+              if (heightOrWidth === this.width) {
+                  num1 = this.pickBetweenTwo(0, this.width)
+                  num2 = this.getRandomNumber(0, this.height)
+              }  else {
+                num1 = this.getRandomNumber(0, this.width)
+                num2 = this.pickBetweenTwo(0, this.height)
+              }
+
+              return {
+                  translateX: num1 - x,
+                  translateY: num2 - y
+              }
+            },
             onResize () {
                 this.width = window.innerWidth
                 this.height = window.innerHeight
@@ -35,7 +55,7 @@
             },
             drawPatterns () {
                 let colors = ['#fff', '#42aab4', '#bd7a96', '#7896c5', '#cb8f5d']
-                let count = Math.ceil(Math.max(this.width, this.height) / 10)
+                let count = Math.ceil(Math.max(this.width, this.height) / 12)
 
                 for (let i = 0; i < count; i++) {
                     let path = svgs[this.getRandomNumber(0, svgs.length - 1)]
@@ -44,6 +64,8 @@
                     let y = this.getRandomNumber(0, this.height)
                     let rotation = this.getRandomNumber(0,360)
 
+                    let { translateX, translateY } = this.getTranslationCoordinates(x, y)
+                    let animation = Raphael.animation({transform: `...T${translateX},${translateY}`}, 600000, 'linear')
 
                     this.paper.path(path)
                         .attr({
@@ -52,10 +74,11 @@
                             'stroke-width': 1
                         })
                         .translate(x, y)
-                        .rotate(rotation)
                         .scale(0.1, 0.1, 0, 0)
+                        .rotate(rotation)
+                        .animate(animation)
                         .glow({color: color, width: 5, offsetx: 6, offsety: 6, opacity: 0.2})
-
+                        .animate(animation)
                 }
             },
             redrawPatterns () {
