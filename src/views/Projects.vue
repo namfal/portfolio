41931c0d0ba98edf510146def4fa352ed52b9dea
Nami Falhan <namfaldev@gmail.com>
2021-04-02 19:52:19 +0200

    added room reports project

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -18,6 +18,14 @@
 				github="https://github.com/namfal/portfolio"
 		>
 		</FlipCard>
+		<FlipCard
+				fileName="roomreports.png"
+				title="Room Reports"
+				link="http://www.roomreports.io/home"
+				text="A project I started to familiarize myself with this particular dev stack and deploying to Heroku. This project ended up being much bigger than I initially though and so it is still a WIP. Since this app is on the free heroku tier, it might be slow to load."
+				techStack="MEVN App"
+		>
+		</FlipCard>
 	</div>
 </template>
 
