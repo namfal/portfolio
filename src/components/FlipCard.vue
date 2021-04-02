e79282df973238caa678bb49b3975dd94af077fd
Nami Falhan <namfaldev@gmail.com>
2021-04-02 19:36:57 +0200

    simplified the flip card animation and completed the info on the crypto watch project

---
+++ src/components/FlipCard.vue
@@ -0,0 +1,153 @@
+<template>
+	<div class="scene">
+		<div :class="[{ 'flipped': cardFlipped}, 'card']">
+			<div class="front" @click="flipCard">
+				<img :src="require(`@/assets/${fileName}`)" alt="Screenshots of a project I worked on">
+				<h4>{{title}}</h4>
+				<div class="see-more"> See More <span class="arrow">-></span> </div>
+			</div>
+			<div class="back">
+				<img :src="require(`@/assets/${fileName}`)" alt="Screenshots of a project I worked on">
+				<h4>{{title}}</h4>
+				<p class="tech-stack">{{techStack}}</p>
+				<p>{{text}}</p>
+				<a :href="link" target="_blank">Live Demo</a>
+				<a :href="github" target="_blank">Github</a>
+				<button @click="flipCard">Close</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'FlipCard',
+	props: ['fileName', 'title', 'text', 'techStack', 'link', 'github'],
+	data () {
+		return {
+			cardFlipped: false
+		}
+	},
+	methods: {
+		flipCard () {
+			this.cardFlipped = !this.cardFlipped
+		}
+	}
+}
+</script>
+
+<style scoped lang="less">
+	.scene {
+		width: 350px;
+		height: 400px;
+		perspective: 600px;
+		padding: 50px;
+	}
+
+	.card {
+		width: 100%;
+		height: 100%;
+		position: relative;
+		transform-style: preserve-3d;
+		transition: transform 1s;
+		border-radius: 20px;
+	}
+
+	.card.flipped {
+		transform: rotateY(180deg);
+		overflow: unset;
+	}
+
+	.front, .back {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		backface-visibility: hidden;
+		border-radius: 20px;
+
+		img {
+			align-self: flex-start;
+		}
+
+		h4 {
+			margin: 10px;
+		}
+	}
+
+	.front {
+		overflow: hidden;
+		display: flex;
+		justify-content: space-between;
+		flex-direction: column;
+
+		img {
+			height: 70%;
+		}
+
+
+		.see-more {
+			align-self: flex-end;
+			margin: 10px 20px;
+
+			.arrow {
+				display: inline-block;
+				transition: transform 200ms ease-in-out;
+			}
+		}
+
+		&:hover {
+			cursor: pointer;
+
+			.arrow {
+				transform: translateX(10px);
+			}
+		}
+	}
+
+	.back {
+		transform: rotateY(180deg);
+		overflow: scroll;
+
+		p, button, a {
+			margin: 10px;
+		}
+
+		.tech-stack {
+			font-size: 1em;
+			border: 1px solid;
+			display: inline-block;
+			padding: 5px;
+			border-radius: 8px;
+			margin: 5px 10px;
+		}
+
+		img {
+			height: 50%;
+		}
+
+		a, a:active, a:visited {
+			color: #000;
+			text-decoration: none;
+
+			&:hover {
+				color: rebeccapurple;
+			}
+		}
+
+		button {
+			margin: 20px auto;
+			display: block;
+			padding: 10px;
+			background-color: white;
+			border: 2px solid black;
+			border-radius: 8px;
+			font-weight: 600;
+			transition: all 100ms ease-in-out;
+
+			&:hover {
+				background-color: black;
+				color: white;
+			}
+		}
+	}
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,52 +1,23 @@
 <template>
 	<div class="container-projects">
-		<div :class="[{'shown' : showBlur}, 'blur']" @click="e => close()"></div>
-		<div class="card" @click="e => expand(e)">
-			<div class="card-child">
-				<div class="front-content">
-					<img src="../assets/crypto-watch.png" alt="">
-					<h4>Crypto Watch</h4>
-				</div>
-				<div class="backside-content">
-					Reverse Content
-					<button @click.stop="e => close(e.target.parentNode.parentNode)">Close</button>
-				</div>
-			</div>
-		</div>
+		<FlipCard
+			fileName="crypto-watch.png"
+			title="Crypto Watch"
+			link="https://tender-bose-c576c3.netlify.app"
+			text="A simple crypto currency price tracker, using Coinbase Pro's Web Hooks API to update prices in real time."
+			techStack="React App"
+			github="https://github.com/namfal/cryptocurrency-watch"
+		>
+		</FlipCard>
 	</div>
 </template>
 
 <script>
+import FlipCard from '../components/FlipCard'
+
 export default {
 	name: 'Projects',
-	data () {
-		return {
-			showBlur: false,
-			currentlyOpenCard: null
-		}
-	},
-	methods: {
-		viewportPercentage (percentage) {
-			return {width: window.innerWidth * percentage / 100, height: window.innerHeight * percentage / 100}
-		},
-		expand (e) {
-			if (this.showBlur) return
-			const {x, y} = e.target.parentNode.getBoundingClientRect()
-			const { width, height } = this.viewportPercentage(70)
-			const translateX = (window.innerWidth / 2) - x - (width / 2)
-			const translateY = (window.innerHeight / 2) - y - (height / 2)
-			e.target.parentNode.classList.toggle('expanded')
-			e.target.style.transform = `translate(${translateX}px, ${translateY}px) rotateY(180deg)`
-			this.showBlur = true
-			this.currentlyOpenCard = e.target
-		},
-		close (element = this.currentlyOpenCard) {
-			element.parentNode.classList.toggle('expanded')
-			element.style.transform = `rotateY(0deg) translate(0px, 0px)`
-			this.showBlur = false
-			this.currentlyOpenCard = null
-		}
-	}
+	components: { FlipCard }
 }
 </script>
 
@@ -58,94 +29,5 @@ export default {
 		justify-content: space-evenly;
 		align-items: flex-start;
 		flex-wrap: wrap;
-	}
-
-	.card {
-		width: 250px;
-		height: 300px;
-		margin: 30px;
-		position: relative;
-		border-radius: 20px;
-		overflow: hidden;
-		transition: all 100ms ease-in-out;
-
-		.card-child {
-			position: absolute;
-			top: 0;
-			left: 0;
-			height: 100%;
-			width: 100%;
-			transition: all 500ms linear;
-
-			.backside-content {
-				display: none;
-			}
-
-			.front-content {
-				height: 100%;
-				overflow: hidden;
-				pointer-events: none;
-
-				img {
-					height: 70%;
-				}
-
-				h4 {
-					margin: 10px;
-				}
-			}
-		}
-
-		&.expanded {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			background-color: transparent;
-			overflow: unset;
-
-			.card-child {
-				position: absolute;
-				top: 0;
-				left: 0;
-				width: 70vw;
-				height: 70vh;
-				pointer-events: auto;
-				background-color: white;
-				overflow: scroll;
-				z-index: 4;
-
-				.backside-content {
-					display: unset;
-					transform: rotateY(180deg);
-					position: absolute;
-					top: 0;
-					right: 0;
-				}
-
-				.front-content {
-					display: none;
-				}
-			}
-		}
-
-		&:hover {
-			cursor: pointer;
-		}
-	}
-
-	.blur {
-		position: absolute;
-		height: 100vh;
-		width: 100vw;
-		top: 0;
-		left: 0;
-		z-index: 3;
-		background: linear-gradient(to bottom right, rgba(0, 8, 17, 0.64), rgba(0, 30, 50, 0.4));
-		backdrop-filter: blur(5px);
-		display: none;
-
-		&.shown {
-			display: unset;
-		}
 	}
 </style>
